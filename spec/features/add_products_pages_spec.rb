26b4c76176f9c9313439174fba5aa75619f5644e
Rafael Furry <rfurry@gmail.com>
2017-11-25 05:12:52 -0800

    add home and about pages

---
+++ spec/features/add_products_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'Apples'
+    fill_in 'Cost', :with => '1.50'
+    fill_in 'Country', :with => 'US'
+    click_on 'Create Product'
+    expect(page).to have_content 'Products'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content 'errors'
+  end
+end
